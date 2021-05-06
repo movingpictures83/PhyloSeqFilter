e298cb97b03cc044cc1334694064975c2dcd7841
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-05-06 10:50:27 -0400

    Initial

---
+++ PhyloSeqFilterPlugin.py
@@ -0,0 +1,99 @@
+import PyPluMA
+
+
+class PhyloSeqFilterPlugin:
+    def input(self, filename):
+      self.parameters = dict()
+      paramfile = open(filename, 'r')
+      for line in paramfile:
+         contents = line.split('\t')
+         self.parameters[contents[0]] = contents[1].strip()
+
+      self.otu_file = open(PyPluMA.prefix()+"/"+self.parameters["OTU"], 'r')
+      self.tax_file = open(PyPluMA.prefix()+"/"+self.parameters["TAX"], 'r')
+      sample_file =open(PyPluMA.prefix()+"/"+self.parameters["META"], 'r')
+      self.threshold = float(self.parameters["threshold"])
+      #Idea: Zero out (don't remove) OTUs in less than 50% of a category set
+      # Then once everything is done, remove those with zero over all samples
+
+      sample_file.readline()
+      cur_sample = "BLAH"
+      cur_sample_idx = -1
+      self.categories = [-1]
+      for line in sample_file:
+         contents = line.strip().split(",")
+         if (contents[1] != cur_sample):
+             cur_sample = contents[1]
+             cur_sample_idx += 1
+         self.categories.append(cur_sample_idx)
+
+    def run(self):
+       pass
+
+    def output(self, filename):
+      otu_filter_file = open(filename+"/"+self.parameters["OTUFilter"], 'w')
+      otu_filter_file.write(self.otu_file.readline())
+
+      toRemove = []
+      for line in self.otu_file:
+          contents = line.strip().split(',')
+          for i in range(0, len(contents)):
+              cat = self.categories[i]
+              if (cat != -1):
+                  if (cat != oldcat):
+                      if ((cat != 0) and (numkittens/numcats < (self.threshold/float(100)))):
+                          # Walk backwards, zero out
+                          j = i-1
+                          while (self.categories[j] == oldcat):
+                              contents[j] = str(0)
+                              j -= 1
+                      #elif (cat != 0):
+                      #    print("KEEPING: "+str(numkittens)+" "+str(numcats))
+                      numkittens = 0.0
+                      numcats = 0.0
+                      if (float(contents[i]) != 0):
+                         numkittens += 1.0
+                      numcats += 1.0
+                  else:
+                      if (float(contents[i]) != 0):
+                         numkittens += 1.0
+                      numcats += 1.0
+              oldcat = cat
+          # One final time
+          if (numkittens/numcats < (self.threshold/float(100))):
+             j = len(contents)-1
+             while (self.categories[j] == oldcat):
+                contents[j] = str(0)
+                j -= 1
+          #else:
+          #   print("KEEPING: "+str(numkittens)+" "+str(numcats))
+
+          # If all are zero, add to remove set
+          zeroFlag = True
+          for i in range(1, len(contents)):
+              if (float(contents[i]) != 0):
+                 zeroFlag = False
+          if (zeroFlag):
+              toRemove.append(contents[0])
+              #print("REMOVING "+contents[0])
+          else:
+              for i in range(0, len(contents)):
+                otu_filter_file.write(contents[i])
+                if (i == len(contents)-1):
+                  otu_filter_file.write('\n')
+                else:
+                  otu_filter_file.write(',')
+      
+      
+      
+      tax_filter_file = open(filename+"/"+self.parameters["TAXFilter"], 'w')
+      tax_filter_file.write(self.tax_file.readline()) #Header
+      
+      
+      for line in self.tax_file:
+          line = line.strip()
+          contents = line.split(',')
+      
+          if (contents[0] not in toRemove):
+              tax_filter_file.write(line+"\n")
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PhyloSeqFilter
-Filter out taxa with certain properties
+# Language: Python
+# Input: TXT (keyword-value pairs)
+# Output: PREFIX
+# Tested with: PluMA 1.1, Python 3.6
+# Dependency:
+
+PluMA plugin that takes PhyloSeq-compatible OTU and TAX tables and
+removes all taxa not present in at least a percentage of the samples,
+which is user-specified.
+
+The plugin takes as input a parameter file of keyword-value pairs:
+OTU: OTU table (input)
+TAX: TAX table (input)
+META: Metadata (input)
+threshold: Minimum percnetage threshold to keep taxa
+OTUFilter: Filtered OTU table (output)
+TAXFilter: Filtered TAX table (output)
+
+OTUFilter and TAXFilter files will be generated with "scarce" taxa
+removed, prefixed by the user-specified PREFIX.
